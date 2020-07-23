4dba1fba10b0d24b18f30e2d2a998379fa5212ed
Thomas <guitaristtom@users.noreply.github.com>
2020-07-22 18:04:48 -0700

    Initial commit

---
+++ bot/__main__.py
@@ -0,0 +1,26 @@
+from discord.ext import commands
+from dotenv import load_dotenv
+from pathlib import Path
+import discord
+import os
+
+# Load up the environment variables
+env_file_name = '.env'
+env_path = Path('.') / env_file_name
+load_dotenv(dotenv_path=env_path)
+
+# Check if .env.local exists, if so, load up those variables, overriding the
+# previously set ones
+local_env_file_name = env_file_name + '.local'
+local_env_path = Path('.') / local_env_file_name
+if os.path.isfile(local_env_file_name):
+    load_dotenv(dotenv_path=local_env_path, override=True)
+
+# Start up the bot
+client = commands.Bot(command_prefix = os.environ['COMMAND_CHARACTERS'])
+
+@client.event
+async def on_ready():
+    print("Bot is ready.")
+
+client.run(os.environ['DISCORD_BOT_TOKEN'])
